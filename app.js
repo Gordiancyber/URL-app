6d2909ac323e60c7ee8ffb8c30ca3f5f42c2daf3
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-08-18 05:34:59 +0000

    commit

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ app.use(bodyParser.json());
 // Create a new instance of the URLShortener class
 const shortener = new URLShortener();
 
+// Define a route to handle URL shortening
+app.post('/shorten', (req, res) => {
+    const url = req.body.url; // Get the URL from the request body
+    if (!url) {
+      return res.status(400).json({ error: 'URL is required.' });
+    }
+  
+    // Shorten the URL using the URLShortener instance
+    const shortURL = shortener.shortenURL(url);
+    res.json({ shortURL }); // Respond with the shortened URL
+  });
+  
+  
