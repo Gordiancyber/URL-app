216e8495757ceb6d23003e7cc61f99204973cc64
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-08-18 04:20:06 +0000

    commit

--- shortener.js
+++ shortener.js
@@ -1,1 +1,15 @@
+// shortener.js
+const crypto = require('crypto');
 
+class URLShortener {
+  constructor() {
+    this.urls = new Map();
+    this.domainMetrics = new Map();
+  }
+
+  generateShortURL(url) {
+    const hash = crypto.createHash('sha1').update(url).digest('hex').substr(0, 6);
+    return hash;
+  }
+
+  
