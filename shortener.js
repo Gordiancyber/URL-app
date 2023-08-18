26ebcb1346350237c8888e10a84f3905aeadd6f5
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-08-18 04:21:05 +0000

    commit

--- shortener.js
+++ shortener.js
@@ -12,4 +12,42 @@ class URLShortener {
     return hash;
   }
 
-  
+  shortenURL(url) {
+    if (this.urls.has(url)) {
+      return this.urls.get(url);
+    }
+
+    const shortURL = this.generateShortURL(url);
+    this.urls.set(url, shortURL);
+    this.recordDomainMetrics(url); // Record domain metrics
+    return shortURL;
+  }
+
+  getOriginalURL(shortURL) {
+    for (const [url, sURL] of this.urls) {
+      if (sURL === shortURL) {
+        return url;
+      }
+    }
+    return null;
+  }
+
+  recordDomainMetrics(url) {
+    const parsedURL = new URL(url);
+    const domain = parsedURL.hostname;
+
+    if (this.domainMetrics.has(domain)) {
+      this.domainMetrics.set(domain, this.domainMetrics.get(domain) + 1);
+    } else {
+      this.domainMetrics.set(domain, 1);
+    }
+  }
+
+  getTopDomains(limit = 3) {
+    const sortedDomains = [...this.domainMetrics.entries()].sort((a, b) => b[1] - a[1]);
+    return sortedDomains.slice(0, limit).map(([domain, count]) => `${domain}: ${count}`);
+  }
+}
+
+module.exports = URLShortener;
+
