5bfec3f800bd0428f8222f3e1b205aa4b600cd69
Gordian <68512769+Gordiancyber@users.noreply.github.com>
2023-08-18 05:35:17 +0000

    commit all

--- app.js
+++ app.js
@@ -23,4 +23,23 @@ app.post('/shorten', (req, res) => {
     res.json({ shortURL }); // Respond with the shortened URL
   });
   
+  // Define a route to handle redirection from short URLs
+  app.get('/:shortURL', (req, res) => {
+    const originalURL = shortener.getOriginalURL(req.params.shortURL); // Get the original URL from the short code
+    if (originalURL) {
+      res.redirect(originalURL); // Redirect the user to the original URL
+    } else {
+      res.status(404).json({ error: 'URL not found.' }); // Return an error if the short URL is not found
+    }
+  });
   
+  // Define a route to get top domains metrics
+  app.get('/metrics/top-domains', (req, res) => {
+    const topDomains = shortener.getTopDomains(); // Get the top domains with the highest usage
+    res.json({ topDomains }); // Respond with the top domains metrics
+  });
+  
+  // Start the Express app and listen on port 8080
+  app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+  });
